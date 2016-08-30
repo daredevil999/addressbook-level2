cc5ef5541da7785f4da02ca58096c31c16de6219
KIANMING-PC\KiANMiNG <kianming@live.com.sg>
2016-08-30 17:46:47 +0800

    Encapsulation of member feedBackToUser in CommandResult class. Edited TextUI class to use the getter instead.

--- src/seedu/addressbook/commands/CommandResult.java
+++ src/seedu/addressbook/commands/CommandResult.java
@@ -11,9 +11,13 @@ import java.util.Optional;
 public class CommandResult {
 
     /** The feedback message to be shown to the user. Contains a description of the execution result */
-    public final String feedbackToUser;
+    private final String feedbackToUser;
 
-    /** The list of persons that was produced by the command */
+    public String getFeedbackToUser() {
+		return feedbackToUser;
+	}
+
+	/** The list of persons that was produced by the command */
     private final List<? extends ReadOnlyPerson> relevantPersons;
 
     public CommandResult(String feedbackToUser) {
